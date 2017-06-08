c7df9b18987528b992e3908c4eb6239bfeac0c5b
canvaser <18721607438@163.com>
2017-06-08 15:28:29 +0800

    desktop+app3

--- desktop/src/main/java/com/summer/desktop/module/app/apps/AppsAdapter.java
+++ desktop/src/main/java/com/summer/desktop/module/app/apps/AppsAdapter.java
@@ -1,13 +1,17 @@
 package com.summer.desktop.module.app.apps;
 
 import android.content.Context;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.summer.desktop.R;
+import com.summer.desktop.bean.dabean.Msg;
 import com.summer.desktop.bean.dbbean.AppDBBean;
 import com.summer.desktop.bean.uibean.AppItemUIBean;
 import com.summer.lib.base.adapter.AppRecycleAdapter;
 import com.summer.lib.util.LogUtil;
+
+import org.greenrobot.eventbus.EventBus;
 
 import java.util.ArrayList;
 
@@ -33,10 +37,21 @@ public class AppsAdapter extends AppRecycleAdapter<AppItemUIBean> {
         holder.itemView.setTag(R.id.data, appDBBeen.get(position));
         holder.getAppicon().setBackgroundDrawable(appDBBeen.get(position).getIcon());
         holder.getTvAppname().setText(appDBBeen.get(position).getAppName());
+        holder.itemView.setOnClickListener(this);
+        holder.itemView.setTag(R.id.data, appDBBeen.get(position));
     }
 
     @Override
     public int getItemCount() {
         return appDBBeen.size();
+    }
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        Msg msg = new Msg();
+        msg.type = AppsAdapter.class.getName();
+        msg.msg = v.getTag(R.id.data);
+        EventBus.getDefault().post(msg);
     }
 }
--- desktop/src/main/java/com/summer/desktop/module/app/apps/AppsFrag.java
+++ desktop/src/main/java/com/summer/desktop/module/app/apps/AppsFrag.java
@@ -6,10 +6,16 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.summer.desktop.bean.dabean.Msg;
 import com.summer.desktop.bean.dbbean.AppDBBean;
 import com.summer.lib.base.fragment.BaseUIFrag;
 import com.summer.lib.base.interf.OnFinishListener;
 import com.summer.lib.base.ope.BaseOpes;
+import com.summer.lib.util.IntentUtil;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
 
@@ -29,5 +35,26 @@ public class AppsFrag extends BaseUIFrag<AppsUIOpe, AppsDAOpe> {
                 getOpes().getUiOpe().initList((ArrayList<AppDBBean>) o);
             }
         });
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void deal(Msg msg) {
+        if (!msg.type.equals(AppsAdapter.class.getName())) {
+            return;
+        }
+        AppDBBean appDBBean = (AppDBBean) msg.msg;
+        IntentUtil.getInstance().IntentTo(activity, appDBBean.getPackageName());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        EventBus.getDefault().unregister(this);
     }
 }
