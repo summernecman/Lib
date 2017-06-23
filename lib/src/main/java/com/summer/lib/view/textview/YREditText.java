4b1fe31318e40e1705d2cccdb7217e4527a6ee64
canvaser <18721607438@163.com>
2017-06-23 09:53:36 +0800

    day and link

--- lib/src/main/java/com/summer/lib/view/textview/YREditText.java
+++ lib/src/main/java/com/summer/lib/view/textview/YREditText.java
@@ -6,11 +6,11 @@ import android.content.Context;
 import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.widget.TextView;
+import android.widget.EditText;
 
-public class YRTextView extends TextView {
+public class YREditText extends EditText {
 
-    public YRTextView(Context context, @Nullable AttributeSet attrs) {
+    public YREditText(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         Typeface mtypeface = Typeface.createFromAsset(context.getAssets(), "yuanrui.ttf");
         setTypeface(mtypeface);
