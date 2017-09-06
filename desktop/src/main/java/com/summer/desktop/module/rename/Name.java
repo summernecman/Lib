66b158a0c6d0f43abb2c06ae3adc213b6cb8c6fc
summernecman <summernecman@163.com>
2017-09-06 13:17:07 +0800

    init

--- desktop/src/main/java/com/summer/desktop/module/rename/Name.java
+++
@@ -1,9 +0,0 @@
-package com.summer.desktop.module.rename;
-
-//by summer on 2017-07-06.
-
-import android.databinding.ObservableField;
-
-public class Name {
-    public final ObservableField<String> name = new ObservableField<>();
-}
